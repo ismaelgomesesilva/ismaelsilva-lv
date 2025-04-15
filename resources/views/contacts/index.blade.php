448572645112e8289031deafe1ba17baa2ccd64f
Ismael Silva <ismaelgomesesilva@gmail.com>
2025-04-15 10:34:40 -0300

    feat: adding edit and delete buttons

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -6,16 +6,25 @@
                 <th>Name</th>
                 <th>Email</th>
                 <th>Contact</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($contacts as $contact)
-                <tr>
-                    <td>{{ $contact->id }}</td>
-                    <td>{{ $contact->name }}</td>
-                    <td>{{ $contact->email }}</td>
-                    <td>{{ $contact->contact }}</td>
-                </tr>
+            <tr>
+                <td>{{ $contact->id }}</td>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->email }}</td>
+                <td>{{ $contact->contact }}</td>
+                <td>
+                    <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-primary">Edit</a>
+                    <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                    </form>
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
