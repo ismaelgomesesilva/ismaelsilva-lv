bc34416327d7106b63cfab18b3776f20a1533db1
Ismael Silva <ismaelgomesesilva@gmail.com>
2025-04-15 10:25:26 -0300

    feat: implement contact listing with pagination in index view

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,3 +1,27 @@
 <div>
-    index
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Contact</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($contacts as $contact)
+                <tr>
+                    <td>{{ $contact->id }}</td>
+                    <td>{{ $contact->name }}</td>
+                    <td>{{ $contact->email }}</td>
+                    <td>{{ $contact->contact }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div>
+        {{ $contacts->links() }}
+    </div>
+
 </div>
