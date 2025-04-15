78508882693fb14717194d7d1f78ee3aef23b8cd
Ismael Silva <ismaelgomesesilva@gmail.com>
2025-04-15 10:10:53 -0300

    feat: implement Contact resource with CRUD methods and views

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('contacts.index');
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Contact $contact)
+    {
+        return view('contacts.show');
+    }
+
+    public function edit(Contact $contact)
+    {
+        return view('contacts.edit');
+    }
+
+    public function update(Request $request, Contact $contact)
+    {
+        //
+    }
+
+    public function destroy(Contact $contact)
+    {
+        //
+    }
 }
